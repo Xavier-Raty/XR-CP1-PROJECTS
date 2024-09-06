a7bee52d5f75b26bf3cc454ea9e4537841dc3ab2
Xavier-Raty <xavier.raty@ucas-edu.net>
2024-09-06 09:48:16 -0600

    create whoAreYou script

---
+++ hello_world/main.py
@@ -0,0 +1,5 @@
+# Xavier Raty, Who Are You?
+
+name = input("What is your name?: ")
+
+print("Hello", name)
---
+++ whoAreYou/main.py
@@ -0,0 +1,13 @@
+#Xavier Raty, Hello World
+
+name = input("What is your name?: ")
+
+print("Hello", name)
+
+age = input("What is your age?: ")
+
+print("You're", age)
+
+color = input("What is your favorite color?: ")
+
+print("Cool, I like", color)
