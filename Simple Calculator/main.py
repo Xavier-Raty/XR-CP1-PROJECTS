7c21b1dcf5ade6663494c5efe5b319eac8953b9a
Xavier-Raty <xavier.raty@ucas-edu.net>
2024-09-25 09:37:25 -0600

    Made code called Simple Calculator

---
+++ Simple Calculator/main.py
@@ -0,0 +1,11 @@
+#Xavier Raty Simple Calculator
+
+number1 = int(input("Give me a number: "))
+number2 = int(input("Give me another number: "))
+print(number1 + number2)
+print(number1 - number2)
+print(number1 * number2)
+print(number1 / number2)
+print(number1 % number2)
+print(number1 ** number2)
+print(number1 // number2)
