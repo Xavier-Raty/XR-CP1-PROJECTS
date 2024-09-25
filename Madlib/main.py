85bf318dd57e9e0642434b44a57762caf7fc7580
Xavier-Raty <xavier.raty@ucas-edu.net>
2024-09-25 09:24:19 -0600

    Made code called Madlib

---
+++ Madlib/main.py
@@ -0,0 +1,8 @@
+# Xavier Raty madlib
+
+noun = input("Give me a noun: ")
+adjective = input("Give me an adjective: ")
+noun2 = input("Give me another noun: ")
+adjective2 = input("Give me another adjective: ")
+verb = input("Give me an verb: ")
+print("There once was a",noun,"and it loved",adjective,noun2,"because it was",adjective2,"It got away because it",verb,"away. The End!")
