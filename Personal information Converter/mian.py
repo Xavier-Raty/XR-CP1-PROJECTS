a5a1228c9d6bdf0b653bd281d7082c4e06a58004
Xavier-Raty <xavier.raty@ucas-edu.net>
2024-09-26 09:36:57 -0600

    Created code called Personal Information Converter

---
+++ Personal information Converter/mian.py
@@ -0,0 +1,9 @@
+# Xavier Raty Personal Information Converter
+
+name = input("What is your name?: ")
+age = input("What is your age?: ")
+height = input("What is your height in meters?: ")
+number = input("What is your favorite number?: ")
+
+print(name,age,height,number)
+print(str(name),int(age),float(height),int(number))
