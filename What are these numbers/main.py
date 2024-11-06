f22cc2cb3e4a790499c31a01a5d5871d9734b7dc
Xavier-Raty <xavier.raty@ucas-edu.net>
2024-11-06 09:23:11 -0700

    edited code called What are these numbers

--- What are these numbers/main.py
+++ What are these numbers/main.py
@@ -4,6 +4,14 @@ num1 = int(input("Give me a number you want converted to the thousands: "))
 string = '{:,.2f}'.format(num1)
 print(string)
 
-num2 = int(input("Give me a number you want with 4 decial places: "))
-string = '{:d}'.format(num2)
+num2 = int(input("Give me a number you want with 4 decimal places: "))
+string = '{:.4f}'.format(num2)
+print(string)
+
+num3 = int(input("Give me a number you want converted into a percentage: "))
+string = '{:.2%}'.format(num3)
+print(string)
+
+num4 = int(input("Give me a number you want in a dollar amount: "))
+string = '${:,.2f}'.format(num4)
 print(string)
