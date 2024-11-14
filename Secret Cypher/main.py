f2ddd25efccf46e1d628a98a3159d6d44cd4ddf0
Xavier-Raty <xavier.raty@ucas-edu.net>
2024-11-14 09:43:26 -0700

    Made code called Secret Cypher

--- Secret Cypher/main.py
+++ Secret Cypher/main.py
@@ -1,2 +1,17 @@
 # Xavier Raty Secret Cypher
 
+alphabet = " abcdeghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ012345678910"
+
+def decode(word):
+    wordLen = len(word)
+    newWord = []
+    repeats = 0
+    for x in word:
+        letter = x 
+        whereLetter = alphabet.index(letter)
+        newWord.insert(repeats, alphabet[whereLetter + 1])
+        word = "".join(newWord)
+        repeats += 1
+    print("decode",word)
+
+decode(input("What word do you want decoded? "))
