d8953c5bbfe32a6c9c4ab7bc41611ec75ac3fd8c
Xavier-Raty <xavier.raty@ucas-edu.net>
2024-11-20 09:35:23 -0700

    made code called Athorized and Count up and down

---
+++ Count up and down/main.py
@@ -0,0 +1,12 @@
+# Xavier Raty Count up and down
+
+import time
+list = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+my_list =[20,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1,0] 
+for i in list :
+    print(i)
+    time.sleep(0.5)
+
+for i in my_list :
+    print (i)
+    time.sleep(0.5)
