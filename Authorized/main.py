e77353a15d52ac5a22736b256a0fc50f1e07ca08
Xavier-Raty <xavier.raty@ucas-edu.net>
2024-11-25 09:39:18 -0700

    made changes to code called authorized

--- Authorized/main.py
+++ Authorized/main.py
@@ -1,2 +1,12 @@
 # Xavier Raty Authorized
 
+list = ["Joe", "Frank", "Bob", "Ms. LaRose", "Jeremy", "Emma", "El Taco", "Sus"]
+list2 = ["Ms. LaRose", "Sus"]
+x = input("What is your username? ")
+if x in list:
+    if x in list2:
+        print("Hello Admin")
+    else:
+        print("Hello authorized user")
+else:
+    print("Your are not authorized")
