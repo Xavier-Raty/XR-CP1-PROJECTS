650b2668fa1846b46378d8f2915e2ba9e59410d3
Xavier-Raty <xavier.raty@ucas-edu.net>
2024-12-02 09:53:02 -0700

    made code called Simple Quiz Game

---
+++ Simple Quiz Game/main.py
@@ -0,0 +1,13 @@
+# Xavier Raty Simple Quiz Game
+
+score = 0
+
+answer_1 = input("What is the Capital of the U.S? A) Washington D.C B) Salt Lake City C) San Fransisco D) Boston:")
+if answer_1 == "A":
+    score += 1
+    print("Correct")
+else: 
+    print("Incorrect. The correct answer is A) Washington D.C.")
+    print("We'll do an easier one next:")
+
+answer_2 = input()
