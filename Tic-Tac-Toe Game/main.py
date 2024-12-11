ef1567c52d16548f536ab380b1f5216c66fb9ba0
Xavier-Raty <xavier.raty@ucas-edu.net>
2024-12-11 09:32:25 -0700

    made edits to code called Tic-Tac-Toe Game

--- Tic-Tac-Toe Game/main.py
+++ Tic-Tac-Toe Game/main.py
@@ -8,21 +8,10 @@ def print_board(board):
         print("-"*5)
 
 def check_winner(board):
-    for row in board:
-        if row[0] == row[1] == row[2] and row[0] != " ":
-            return row[0]
-
-    for col in range(3):
-        if board[0][col] == board[1] == board[2] and board[0] != " ":
-            return board[0][col]
-
-    if board[0][0] == board[1][1] == board[2][2] and board[0][0] != " ":
-            return board[0][0]
-    if board[0][2] == board[1][1] == board[2][0] and board[0][2] != " ":
-            return board[0][2]
-    
-    return None
-
+    for i in range(3):
+        if board[i][0] == board[i][1] == board[i][2] and board[i][0] != " ":
+            return board[i][0]
+        
 def player_move(board):
      while True:
             try:
